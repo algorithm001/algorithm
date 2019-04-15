5e7cfdaa3505381dbefba3394fb51433382ec6f4
guangjun.ma <guangjun.ma@qunar.com>
2019-04-15 18:49:34 +0800

    maguangjun: add test class ,to use github

---
+++ Week_01/id_58/TestClass.java
@@ -0,0 +1,12 @@
+package algorithm.Week_01.id_58;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!"
+
+        );
+    }
+
+}

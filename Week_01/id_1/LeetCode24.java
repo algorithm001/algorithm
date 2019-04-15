5341a468a2b91833f9959eb6377c22a9badc79b9
zouyingjie <yingjiezou1@gmail.com>
2019-04-15 21:39:21 +0800

    week01:leetcode24-Swap Nodes in Pairs

---
+++ Week_01/id_1/LeetCode24.java
@@ -0,0 +1,48 @@
+
+class Solution {
+
+    ListNode prePairTail = null;
+
+    public ListNode swapPairs(ListNode head) {
+
+
+        if (head == null) {
+            return null;
+        }
+
+        if (head.next == null) {
+            return head;
+        }
+
+        ListNode result = head.next;
+        
+        ListNode node = head;
+        ListNode next = head.next;
+
+        while (node != null && next != null) {
+
+            node = swap(node, next);
+
+            if (node == null) {
+                break;
+            }
+            next = node.next;
+
+        }
+        return result;
+
+    }
+
+    private ListNode swap(ListNode node, ListNode next) {
+
+        ListNode tmpNext = next.next;
+        next.next = node;
+        node.next = tmpNext;
+        if (prePairTail != null) {
+            prePairTail.next = next;
+        }
+        prePairTail = node;
+        return tmpNext;
+    }
+
+}

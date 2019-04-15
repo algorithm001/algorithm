6083701eee7461b1b808e2931c0454fed73229b5
zouyingjie <yingjiezou1@gmail.com>
2019-04-15 22:58:37 +0800

    001 号第一周作业: LeetCode25- Reverse Nodes in k-Group

--- Week_01/id_1/LeetCode25.java
+++ Week_01/id_1/LeetCode25.java
@@ -16,7 +16,7 @@ class Solution {
 
         while (true) {
 
-            if (index != k) {
+            if (index != k) { // 查找 k 个节点中的 head 和 tail 节点
                 groupTail = groupTail.next;
                 if (groupTail != null) {
                     index ++;
@@ -30,7 +30,8 @@ class Solution {
                         return head;
                     }
                 }
-            }else {
+            }else { 
+                // 个数达到 k 时执行交换，返回下一组的 head 节点
                 ListNode node = this.swap(groupHead, groupTail);
                 if (node == null) {
                     break;
@@ -45,6 +46,7 @@ class Solution {
 
     private ListNode swap(ListNode head, ListNode tail) {
 
+        // 反转链表
         ListNode preNode = null;
         ListNode currentNode = head;
         while (currentNode != tail) {
@@ -54,12 +56,17 @@ class Solution {
             currentNode = next;
         }
 
+        // 当前组的 tail 节点执行 swap
         ListNode node= currentNode.next;
         currentNode.next = preNode;
 
+        // 反转完成，将上一组的 tail 节点指向当前组的 head 节点
+        // 第一次反转时上一组 tail 为 null，因此要执行判空操作
         if (this.preGroupTail != null) {
             this.preGroupTail.next = currentNode;
         }else {
+            // 上一组的 tail 为空说明是第一组执行交换
+            // 结果一定是第一组交换后的 head 节点
             result = currentNode;
         }
         this.preGroupTail = head;

7b0ed257f4d86976c4622495bfde2dd4c61177a4
DaveCheng <34067983+cwbcheng@users.noreply.github.com>
2019-04-17 22:23:35 +0800

    https://leetcode-cn.com/problems/swap-nodes-in-pairs/comments/

---
+++ Week_01/id_2/两两交换链表中的节点.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode.两两交换链表中的节点
+{
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class 两两交换链表中的节点
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            ListNode a = new ListNode(int.MinValue);
+            ListNode b = head;
+            ListNode c = head?.next;
+            a.next = head;
+            head = a;
+
+            while (b != null && c != null)
+            {
+                var temp = c.next;
+                c.next = b;
+                b.next = temp;
+                a.next = c;
+                a = b;
+                b = b?.next;
+                c = b?.next;
+            }
+            head = head.next;
+
+            return head;
+        }
+
+        public ListNode SwapPairs_recursive(ListNode head)
+        {
+            if (head == null || head.next == null)
+            {
+                return null;
+            }
+
+            var next = head.next;
+            head.next = SwapPairs_recursive(next.next);
+            next.next = head;
+            return next;
+        }
+
+        public void Printf(ListNode ListNode)
+        {
+            while (ListNode != null)
+            {
+                Console.WriteLine(ListNode.val);
+                ListNode = ListNode.next;
+            }
+        }
+    }
+}

7f00b0f7669aca5d384ad282b8dc1336464d1cc5
乐牙 <liuyunjian@alibaba-inc.com>
2019-04-18 20:45:32 +0800

    binary tree's longest-univalue-path. use recursion

---
+++ Week_01/id_14/TreeNode.java
@@ -0,0 +1,16 @@
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) { val = x; }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("val:").append(val)
+            .append(" -left:").append(left == null ? -1 : left.val)
+            .append(" -right:").append(right == null ? -1 : right.val);
+        return stringBuilder.toString();
+    }
+}

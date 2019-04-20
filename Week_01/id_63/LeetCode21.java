4e427909fb346b0f0e3069db596f262dc66d2350
wahaha <48761625+wahaha2019@users.noreply.github.com>
2019-04-20 08:13:01 +0000

    Update LeetCode21.java

--- Week_01/id_63/LeetCode21.java
+++ Week_01/id_63/LeetCode21.java
@@ -1,3 +1,7 @@
+/*
+https://leetcode-cn.com/problems/merge-two-sorted-lists/
+将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+*/
 /**
  * Definition for singly-linked list.
  * public class ListNode {

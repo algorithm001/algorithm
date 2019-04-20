a46f761322ed1a65ccd817e92ca6a4bc82a298a6
wahaha <48761625+wahaha2019@users.noreply.github.com>
2019-04-20 08:15:02 +0000

    Update LeetCode142.java

--- Week_01/id_63/LeetCode142.java
+++ Week_01/id_63/LeetCode142.java
@@ -1,3 +1,9 @@
+/*
+https://leetcode-cn.com/problems/linked-list-cycle-ii/
+给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+说明：不允许修改给定的链表。
+*/
 /**
  * Definition for singly-linked list.
  * class ListNode {

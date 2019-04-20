89ebf2e4a9b9050f8bc31c3c163f8b42dd9e8be7
tidelgl <tidelgl@163.com>
2019-04-20 22:08:11 +0800

    add second solution

--- Week_01/id_64/LeetCode_64_21.js
+++ Week_01/id_64/LeetCode_64_21.js
@@ -42,3 +42,24 @@ var mergeTwoLists = function(l1, l2) {
     
     return dummyHead.next;
 };
+
+/**
+ * 21. 合并两个有序链表
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ * 递归实现 
+ *
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(l1, l2) {
+    // 返回非空的链表
+    if (l1 == null || l2 == null) { return l1 ? l1 : l2; }
+    if (l1.val < l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l2.next, l1);
+        return l2;
+    } 
+};

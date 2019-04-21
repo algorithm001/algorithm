b3a2a9cf2d8cdffc1de20a68c6b5957c6debb8c3
tidelgl <tidelgl@163.com>
2019-04-21 18:06:35 +0800

    add slow and fast index to 141

--- Week_01/id_64/LeetCode_141_64.js
+++ Week_01/id_64/LeetCode_141_64.js
@@ -28,3 +28,24 @@ var hasCycle = function(head) {
     
     return false;
   };
+
+  /**
+ * 141. 环形链表
+ * https://leetcode-cn.com/problems/linked-list-cycle/
+ * 使用快慢指针, 当快指针在环里打转碰上慢指针即检测到环
+ * 
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    let slow = head;
+    let fast = head;
+    while (fast != null) {
+      if (fast.next == null) { return false; }
+      fast = fast.next.next;
+      slow = slow.next;
+      if (fast == slow) return true;
+    } 
+    
+    return false;
+  };

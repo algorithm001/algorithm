0bc7965cd0226b2a1c367d5673b9b0b8cc743278
gongwentao <326967648@qq.com>
2019-04-21 13:29:49 +0800

    效率高的一种解法

--- Week_01/id_104/LeetCode_242_104.java
+++ Week_01/id_104/LeetCode_242_104.java
@@ -11,31 +11,21 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        Map<Character,Integer> sMaps=new HashMap();
-        Map<Character,Integer> tMaps=new HashMap();
-        //默认值
-        int defaultNum=0;
-        Character tmpChar;
-        Integer v;
-        // 初始化字典集合 用空间换时间
+        boolean result=true;
+        int[] sAry=new int[26];
+        int[] tAry=new int[26];
         for(int i=0;i<s.length();i++){
-            Integer n=sMaps.get(s.charAt(i));
-            sMaps.put(s.charAt(i),(n==null?1:++n));
+            sAry[s.charAt(i)-'a']++;
+            tAry[t.charAt(i)-'a']++;
         }
-        // 3.遍历字符串，判断其中字符是否在另外一个字符串中存在，不存在则退出
-        for(int i=0;i<t.length();i++){
-            tmpChar=t.charAt(i);
-            v=sMaps.get(tmpChar);
-            if(v==null ){
+        for(int i=0;i<26;i++){
+            if(sAry[i]!=tAry[i]){
+                result=false;
                 break;
             }
-            v--;
-            sMaps.put(tmpChar,v);
-            if(v==0){
-                sMaps.remove(tmpChar);
-            }
         }
-        System.out.println(sMaps.size());
-        return sMaps.size()==0;
+        
+        return result;
+        
     }
-}g
+}

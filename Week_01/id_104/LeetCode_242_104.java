12a04c6c160ecb2a3462d9a05d30b5d92db4aee4
gongwentao <326967648@qq.com>
2019-04-21 13:29:15 +0800

    少一个词典解法

--- Week_01/id_104/LeetCode_242_104.java
+++ Week_01/id_104/LeetCode_242_104.java
@@ -11,30 +11,31 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        boolean result=true;
         Map<Character,Integer> sMaps=new HashMap();
         Map<Character,Integer> tMaps=new HashMap();
         //默认值
         int defaultNum=0;
+        Character tmpChar;
+        Integer v;
         // 初始化字典集合 用空间换时间
         for(int i=0;i<s.length();i++){
             Integer n=sMaps.get(s.charAt(i));
             sMaps.put(s.charAt(i),(n==null?1:++n));
-            Integer b=tMaps.get(t.charAt(i));
-            tMaps.put(t.charAt(i),b==null?1:++b);
         }
         // 3.遍历字符串，判断其中字符是否在另外一个字符串中存在，不存在则退出
-        for(Map.Entry<Character,Integer> entry:sMaps.entrySet()){
-            Integer r=tMaps.get(entry.getKey());
-      
-            if(r==null || !entry.getValue().equals(r) ){
-                System.out.println("key:"+entry.getKey()+" v:"+entry.getValue()+" rv:"+r+" equal:"+(entry.getValue().equals(r)));
-                result=false;
+        for(int i=0;i<t.length();i++){
+            tmpChar=t.charAt(i);
+            v=sMaps.get(tmpChar);
+            if(v==null ){
                 break;
             }
+            v--;
+            sMaps.put(tmpChar,v);
+            if(v==0){
+                sMaps.remove(tmpChar);
+            }
         }
-        return result;
-
-        
+        System.out.println(sMaps.size());
+        return sMaps.size()==0;
     }
-}
+}g

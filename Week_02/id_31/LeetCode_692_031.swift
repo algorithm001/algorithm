555cd402a5524999b92e815eeba17adab7aeefbb
CRGT_Ghcoder <huan.gong@crgecent.com>
2019-04-22 21:48:26 +0800

    添加相关的注释，为了别人更好理解代码

--- Week_02/id_31/LeetCode_692_031.swift
+++ Week_02/id_31/LeetCode_692_031.swift
@@ -10,25 +10,29 @@ import Foundation
 
 class Solution {
     func topKFrequent(_ words: [String], _ k: Int) -> [String] {
-        var collect: [String: (word: String, count: Int)] = [:]
-        var index = 0
+        // 定义一个哈希Map来存放数据，key: 单词，value: (首字母，单词出现的个数)
+        var collect: [String: (char: String, count: Int)] = [:]
         var sortedResetCollect: [String] = []
+        // 第一次遍历，统计单词出现的次数、首字母
         for i in 0...words.count - 1 {
             let w = words[i]
             if var t = collect[words[i]] {
                 t.count += 1
                 collect[words[i]] = t
             } else {
-                collect[words[i]] = (word: w, count: 1)
+                collect[words[i]] = (char: w, count: 1)
             }
         }
+        // 针对这个哈希Map排序，情况1：将单词出现数量多的排在前面   情况2：如果出现的数量相同的情况下，比较首字母
         let sortedCollect = collect.sorted {
             if $0.value.count != $1.value.count {
                 return $0.value.count > $1.value.count
             } else {
-                return $0.value.word < $1.value.word
+                return $0.value.char < $1.value.char
             }
         }
+        // 只取前K个元素，用局部变量Index来统计数量
+        var index = 0
         for (key, _) in sortedCollect {
             if index >= k { break }
             sortedResetCollect.append(key)

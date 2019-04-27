155783a2a954a0d1360f85e0e4db92e4ab1ecd50
haoge <864706450@qq.com>
2019-04-27 19:56:38 +0800

    week2-作业

---
+++ Week_02/id_143/Leetcode692_143.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Leetcode692_143 {
+
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String,Integer> map=new HashMap<>();
+        for (String s :words)
+        {
+            map.put(s,map.getOrDefault(s,0)+1);
+        }
+        PriorityQueue<Map.Entry<String,Integer>> queue=new PriorityQueue<>(new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if(o1.getValue()==o2.getValue())
+                {
+                    return o1.getKey().compareTo(o2.getKey());
+                }
+                return o2.getValue()-o1.getValue();
+            }
+        });
+        queue.addAll(map.entrySet());
+        List<String> res=new ArrayList<>(k);
+        for (int i=0;i<k;i++)
+        {
+            res.add(queue.poll().getKey());
+        }
+        return res;
+
+
+    }
+}

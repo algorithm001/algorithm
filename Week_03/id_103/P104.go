da7bddc882c5bfef75f2681a2ba50c0deeb4140b
姜雨生 <jiangyusheng@jiangyuhengsMBP.lan>
2019-05-04 13:38:15 +0800

    Week 3

---
+++ Week_03/id_103/P104.go
@@ -0,0 +1,20 @@
+package main
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	left := maxDepth(root.Left) + 1
+	right := maxDepth(root.Right) + 1
+	if left > right {
+		return left
+	}
+	return right
+}
---
+++ Week_03/id_103/TreeNode.go
@@ -0,0 +1,9 @@
+package main
+
+
+type TreeNode struct {
+     Val int
+     Left *TreeNode
+     Right *TreeNode
+}
+

ca59e339792dd9b6ba7be31a0847a4b0dafcce27
姜雨生 <jiangyusheng@jiangyuhengsMBP.lan>
2019-05-04 14:50:12 +0800

    note.md

--- Week_03/id_103/P373.go
+++ Week_03/id_103/P373.go
@@ -38,7 +38,7 @@ func kSmallestPairs(nums1 []int, nums2 []int, k int) [][]int {
 	heap.Init(v)
 	for _,v1 := range nums1 {
 		for _,v2 := range nums2 {
-			if v.Len() < k {
+			if v.Len() < k {// 这里不能都添加完在移除，因为在pop哪里，移除方法并不是移除最大的，而是最后一个，如果保证堆内的数量，则可以保证，移除的一定是最大的。
 				heap.Push(v,[]int{v1,v2})
 			}else {
 				heap.Push(v,[]int{v1,v2})

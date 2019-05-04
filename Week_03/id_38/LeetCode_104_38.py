735bb7b8c4be19eb12f09e3cf2a1a74f17e4ef77
Stephen Chiang <stephenchiang@yahoo.com>
2019-05-04 23:53:05 +0800

    Week_03

---
+++ Week_03/id_38/LeetCode_104_38.py
@@ -0,0 +1,118 @@
+class Solution:
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return self.get_tree_max_depth(root, 0)
+
+    def get_tree_max_depth(self, root, depth):
+        if not root:
+            return depth
+        return max(self.get_tree_max_depth(root.left, depth), self.get_tree_max_depth(root.right, depth)) + 1
+
+
+class Solution:
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return self.get_tree_max_depth(root, 0)
+
+    def get_tree_max_depth(self, root, depth):
+        if not root:
+            return depth
+        return max(self.get_tree_max_depth(root.left, depth), self.get_tree_max_depth(root.right, depth)) + 1
+
+
+class Solution:
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return self.get_tree_max_depth(root, 0)
+
+    def get_tree_max_depth(self, root, depth):
+        if not root:
+            return depth
+        return max(self.get_tree_max_depth(root.left, depth), self.get_tree_max_depth(root.right, depth)) + 1
+
+
+class Solution:
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return self.get_tree_max_depth(root, 0)
+
+    def get_tree_max_depth(self, root, depth):
+        if not root:
+            return depth
+        return max(self.get_tree_max_depth(root.left, depth), self.get_tree_max_depth(root.right, depth)) + 1
+
+
+class Solution:
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return self.get_tree_max_depth(root, 0)
+
+    def get_tree_max_depth(self, root, depth):
+        if not root:
+            return depth
+        return max(self.get_tree_max_depth(root.left, depth), self.get_tree_max_depth(root.right, depth)) + 1
+
+
+class Solution:
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return self.get_tree_max_depth(root, 0)
+
+    def get_tree_max_depth(self, root, depth):
+        if not root:
+            return depth
+        return max(self.get_tree_max_depth(root.left, depth), self.get_tree_max_depth(root.right, depth)) + 1
+
+
+class Solution:
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return self.get_tree_max_depth(root, 0)
+
+    def get_tree_max_depth(self, root, depth):
+        if not root:
+            return depth
+        return max(self.get_tree_max_depth(root.left, depth), self.get_tree_max_depth(root.right, depth)) + 1
+
+
+class Solution:
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return self.get_tree_max_depth(root, 0)
+
+    def get_tree_max_depth(self, root, depth):
+        if not root:
+            return depth
+        return max(self.get_tree_max_depth(root.left, depth), self.get_tree_max_depth(root.right, depth)) + 1
+
+
+class Solution:
+
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return self.get_tree_max_depth(root, 0)
+
+    def get_tree_max_depth(self, root, depth):
+        if not root:
+            return depth
+        return max(self.get_tree_max_depth(root.left, depth), self.get_tree_max_depth(root.right, depth)) + 1
+
+
+
---
+++ Week_03/id_38/LeetCode_703_38.py
@@ -0,0 +1,19 @@
+import heapq
+
+
+class KthLargest:
+    def __init__(self, k, nums):
+        self.nums = nums
+        self.k = k
+        heapq.heapify(self.nums)
+        while len(self.nums) > k:
+            heapq.heappop(self.nums)
+
+    def add(self, val):
+        if len(self.nums) < self.k:
+            heapq.heappush(self.nums, val)
+        elif val > self.nums[0]:
+            heapq.heapreplace(self.nums, val)
+        return self.nums[0]
+
+

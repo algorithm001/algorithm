b8f1ef6d9543a996bf6ebdb7930c3d6eb75c321a
XinRuoShi <XinruoShi>
2019-05-05 10:03:06 +0800

    week 3/4 push

---
+++ Week_04/id_86/LeetCode_169_086.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmTest
+{
+    class LeetCode_169_086
+    {
+        public int MethodFor169_1(int[] num)
+        {
+            int n = num[0];
+            int count = 1;
+
+            for (int i = 1; i < num.Length; i++)
+            {
+                if (count == 0)
+                {
+                    count++;
+                    n = num[i];
+                }
+                else if (n == num[i])
+                {
+                    count++;
+                }
+                else count--;
+
+            }
+            return n;
+        }
+
+        //直接调用 Array.Sort
+        public int MethodFor169_2(int[] num)
+        {
+            Array.Sort(num);
+            return num[num.Length / 2];
+        }
+    }
+}
---
+++ Week_04/id_86/LeetCode_746_086.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmTest
+{
+    class LeetCode_746_086
+    {
+        public int MethodFor746_1(int[] cost)
+        {
+            for (int i = 2; i < cost.Length; i++)
+            {
+                cost[i] += Math.Min(cost[i - 2], cost[i - 1]);
+            }
+
+            return Math.Min(cost[cost.Length - 1], cost[cost.Length - 2]);
+        }
+    }
+}

360146e476a86011c08002e809dbd9f3752d2f21
v1xingyue <qixingyue@126.com>
2019-05-07 14:03:25 +0800

    720 解题架构

---
+++ Week_04/id_7/LeetCode_720_7.go
@@ -0,0 +1,61 @@
+package geekcode
+
+// 存储节点数据
+type tries_node struct {
+	c        byte
+	children map[byte]*tries_node
+	gocount  int // 经过的单词个数
+}
+
+// 存储结果数据
+type find_result struct {
+	path      string // 路径用来返回结果
+	level     int    // 层级用来比较大小
+	last_byte byte   // 最后一个字节长度，用来获取最大的字典序
+}
+
+//往tries 树中添加一个单词
+func (self *tries_node) addString(c string) {
+	last := self
+	for _, b := range c {
+		if last.children == nil {
+			last.children = make(map[byte]*tries_node)
+		}
+
+		n := &tries_node{c: byte(b), children: nil}
+		last.children[byte(b)] = n
+		last = n
+	}
+}
+
+//使用字典创建tries 树，获取深度最长的叶子节点
+func longestWord(words []string) string {
+
+	result := make([]find_result, 0)
+	root_tree := new(tries_node)
+
+	// 每个单词添加到tries 树中
+	for _, v := range words {
+		root_tree.addString(v)
+	}
+
+	goon := true
+
+	// 处理运算结果
+	if len(result) == 0 {
+		return ""
+	} else if len(result) == 1 {
+		return result[0].path
+	} else {
+		var max_byte byte
+		max_byte = 0
+		result_string := ""
+		for _, v := range result {
+			if max_byte < v.last_byte {
+				max_byte = v.last_byte
+				result_string = v.path
+			}
+		}
+		return result_string
+	}
+}
---
+++ Week_04/id_7/readme.md
@@ -0,0 +1,4 @@
+# 学习笔记
+
+* [词典中最长的单词](https://leetcode-cn.com/problems/longest-word-in-dictionary/) 
+  [LeetCode_720_7.go](LeetCode_720_7.go)

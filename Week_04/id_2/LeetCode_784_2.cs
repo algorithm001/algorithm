5fe3fe2e31c784d8473fa8d9678789bcbb6ababb
DaveCheng <34067983+cwbcheng@users.noreply.github.com>
2019-05-12 23:33:10 +0800

    Update LeetCode_784_2.cs

--- Week_04/id_2/LeetCode_784_2.cs
+++ Week_04/id_2/LeetCode_784_2.cs
@@ -1,26 +1,28 @@
-public class Solution {
+public class Solution
+{
     List<string> result = new List<string>();
-    public IList<string> LetterCasePermutation(string S) {
-        
-        Helper(S,"",0);
-           
-        return result;
-    }    
-    public void Helper(string s,string cur,int i)
+    public IList<string> LetterCasePermutation(string S)
     {
-        if(i == s.Length)
+
+        Helper(S, "", 0);
+
+        return result;
+    }
+    public void Helper(string s, string cur, int i)
+    {
+        if (i == s.Length)
         {
             result.Add(cur);
             return;
         }
-         if(char.IsDigit(s[i]))
-           {
-                Helper(s,cur+s[i],i+1);
-           }
-            else
-            {
-                Helper(s,cur+char.ToLower(s[i]),i+1);
-                Helper(s,cur+char.ToUpper(s[i]),i+1);
-            }
+        if (char.IsDigit(s[i]))
+        {
+            Helper(s, cur + s[i], i + 1);
+        }
+        else
+        {
+            Helper(s, cur + char.ToLower(s[i]), i + 1);
+            Helper(s, cur + char.ToUpper(s[i]), i + 1);
+        }
     }
 }

86825c4a170585b6d4426b877566c398d476ae52
jiangchuan.deng@okcoin.com <jiangchuan.deng@okcoin.com>
2019-05-12 21:37:30 +0800

    week four

---
+++ Week_04/id_72/LeetCode_746.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int length = cost.length + 1;
+        int dp0 = 0;
+        int dp1 = 0;
+        int dp2 = 0;
+        for (int i = 2; i < length; i++) {
+            dp2 = Math.min(dp0 + cost[i - 2] , dp1 + cost[i - 1]);
+            dp0 = dp1;
+            dp1 = dp2;
+        }
+        return dp2;
+    }
+}

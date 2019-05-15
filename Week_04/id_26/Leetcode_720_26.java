c178caf70b27d8665a920fc32b9b47057b7326f0
fanlu91 <fanlu91@gmail.com>
2019-05-15 21:13:42 +0800

    update solution

--- Week_04/id_26/Leetcode_720_26.java
+++ Week_04/id_26/Leetcode_720_26.java
@@ -1,67 +1,98 @@
 package com.fanlu.leetcode.trietree;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.HashMap;
+import java.util.Stack;
 
 // Source : https://leetcode.com/problems/longest-word-in-dictionary/
 // Id     : 720
 // Author : Fanlu Hai
 // Date   : 2018-05-12
-// Other  :
+// Other  : children list will affect the performance
 // Tips   :
 public class LongestWordInDictionary {
-    TrieNode root = new TrieNode("/");
-//    String longest = null;
+    TrieNode root = new TrieNode('/');
+    String longest = "";
 
-    public void insert(String string) {
+    public void insert(String string, int index) {
         TrieNode node = root;
-        for (int i = 0; i < string.length(); i++) {
-            int index = string.charAt(i) - 'a';
-            if (null == node.children[index]) {
-                node.children[index] = new TrieNode();
-            }
-            node = node.children[index];
+        for (char c : string.toCharArray()) {
+            node.children.putIfAbsent(c, new TrieNode(c));
+            node = node.children.get(c);
         }
-        node.isWord = true;
-        node.word = string;
+        // not only means this node is the end of a word,but also contains index information for this word.
+        node.index = index;
     }
 
+    // 34.81%(22 ms) 97.45%
+    public String longestWordSlow(String[] words) {
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i], i);
+            // System.out.println("insert: " + i + " " + words[i]);
+        }
+
+        Stack<TrieNode> stack = new Stack();
+        stack.addAll(root.children.values());
+
+        TrieNode node;
+        while (!stack.isEmpty()) {
+            node = stack.pop();
+            // System.out.println(node.c+" "+node.index);
+            if (node.index != -1) {
+                String tmp = words[node.index];
+                // System.out.println("check "+tmp);
+                if (tmp.length() > longest.length()
+                        || (tmp.length() == longest.length() && tmp.compareTo(longest) < 0)) {
+                    // System.out.println("longest "+tmp);
+                    longest = tmp;
+                }
+                stack.addAll(node.children.values());
+            }
+
+        }
+
+        return longest;
+    }
+
+    //51.26% (16 ms) 98.36%
     public String longestWord(String[] words) {
-        for (String word : words) {
-            insert(word);
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i], i);
+            // System.out.println("insert: " + i + " " + words[i]);
         }
-        Deque<TrieNode> st = new ArrayDeque<>();
-        st.push(root);
-        String best = "";
-
-        while (!st.isEmpty()) {
-            TrieNode nd = st.pop();
-            if (nd.word != null && nd.word.length() > best.length())
-                best = nd.word;
-            if (null == nd.children)
-                continue;
-            for (char c = 'z'; c != 'a' - 1; --c) {
-                TrieNode ch = nd.children[c - 'a'];
-                if (null == ch || ch.word == null)
-                    continue;
-                st.push(ch);
-            }
+        for (TrieNode child : root.children.values()) {
+            searchLongest(child, words);
         }
-        return best;
+        return longest;
     }
 
-    public class TrieNode {
-        public String word;
-        public TrieNode[] children = new TrieNode[26];
-        public boolean isWord = false;
+    public void searchLongest(TrieNode node, String[] words) {
+        if (node.index != -1) {
+            String tmp = words[node.index];
+            // System.out.println("check "+tmp);
+            if (tmp.length() > longest.length()
+                    || (tmp.length() == longest.length() && tmp.compareTo(longest) < 0)) {
+                // System.out.println("longest "+tmp);
+                longest = tmp;
+            }
+            for (TrieNode child : node.children.values()) {
+                searchLongest(child, words);
+            }
+        }
+    }
 
-        public TrieNode(String word) {
-            this.word = word;
+    class TrieNode {
+        public char c;
+        // to keep track which word's insertion created this node
+        public int index = -1;
+        HashMap<Character, TrieNode> children = new HashMap();
+
+        public TrieNode(char c) {
+            this.c = c;
         }
 
-        public TrieNode() {
-
+        public TrieNode(char c, int index) {
+            this.c = c;
+            this.index = index;
         }
-
     }
 }

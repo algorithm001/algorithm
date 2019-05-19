7ad20a2b51e57a12b536f9d3dfc9cdce14b2beaf
luckypeak <luckypeak>
2019-05-19 23:41:19 +0800

    第四周提交

---
+++ Week_04/id_124/LeetCode_746_124.java
@@ -0,0 +1,19 @@
+public class MinCostClimbingStairs {
+
+    public int minCostClimbingStairs(int[] cost) {
+        if (cost.length == 2) {
+            return Math.min(cost[0], cost[1]);
+        }
+        int[] dp = new int[cost.length + 1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        for (int i = 2; i < cost.length; i++) {
+            dp[i] = Math.min(dp[i - 2], dp[i - 1]) + cost[i];
+        }
+        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+    }
+
+
+
+
+}
